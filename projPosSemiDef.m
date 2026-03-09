9482eca2945623f8ba2c2bcf5e2a74c39f26a234
Shabnam Khodakaramzadeh <91033861+skhodakaram@users.noreply.github.com>
2026-03-09 17:27:58 +0100

    update

--- projPosSemiDef.m
+++ projPosSemiDef.m
@@ -1,6 +1,11 @@
-% Author: Shabnam Khodakaramzadeh, researcher at TU Delft
 function [MatProj]= projPosSemiDef(Mat) 
 % Projection on the positive semidefinite cone 
+    [V, D] = eig(Mat);
+    d = max(0,diag(D));
+    VDh = V*diag(max(0,sqrt(d)));
+    MatProj = VDh*VDh';
+    return;
+
     n= size(Mat,1);
     [eigVec,eigVal]= eig(Mat);
     if norm(imag(eigVal)) > 10^-8
--- projection_to_S2.m
+++ projection_to_S2.m
@@ -1,16 +1,21 @@
 function Lambda = projection_to_S2(Lambda)
-    n= size(Lambda, 1);
-    for i= 1 : n
-        [v_max, d_max] = eigs(Lambda, 1, 'largestreal', 'Tolerance', 1e-6);
-        if isnan(d_max) % d_max == NaN
-            [eigVec,eigVal]= eVec_eVal_sort_Descending(Lambda);
-            v_max= eigVec(:,1);
-            d_max= eigVal(1,1);
-        end
-        if d_max <= 1 + 1e-6
-            break
-        else
-            Lambda = Lambda + (1 - d_max) * (v_max * v_max');
+    n = length(Lambda);
+    if n <= 1000  % For moderate sizes, use normal eig
+        [V, D] = eig(Lambda);
+        Lambda = V*diag(min(diag(D),1))*V';
+    else
+        for i= 1 : n
+            [v_max, d_max] = eigs(Lambda, 1, 'largestreal', 'Tolerance', 1e-6);
+            if isnan(d_max) % d_max == NaN
+                [eigVec,eigVal]= eVec_eVal_sort_Descending(Lambda);
+                v_max= eigVec(:,1);
+                d_max= eigVal(1,1);
+            end
+            if d_max <= 1 + 1e-6
+                break
+            else
+                Lambda = Lambda + (1 - d_max) * (v_max * v_max');
+            end
         end
     end
 end
